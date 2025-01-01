3bd49a840a11653a8cfb7d42b628b2665cb47264
erick-augusto <erick_aug@hotmail.com>
2025-01-01 12:43:07 -0300

    Filtrando mais informações com novas rotas

--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -31,5 +31,8 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     List<Episodio> episodiosPorSerieEAno(Serie serie, int anoLancamento);
 
     @Query("SELECT s FROM Serie s JOIN s.episodios e GROUP BY s ORDER BY MAX(e.dataLancamento) DESC LIMIT 5")
-    List<Serie> findTop5ByOrderByEpisodiosDataLancamentoDesc();
+    List<Serie> lancamentosMaisRecentes();
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s.id = :id AND e.temporada = :numero")
+    List<Episodio> obterEpisodiosPorTemporada(Long id, Long numero);
 }
